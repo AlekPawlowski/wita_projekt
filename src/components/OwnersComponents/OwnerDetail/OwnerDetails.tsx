385a96faccb1d2ae1adc64233b7e61b9746e7cd7
AlekPawlowski <alekpawlowski21@gmail.com>
2023-09-05 19:19:57 +0200

    owner details progress

--- src/components/OwnersComponents/OwnerDetail/OwnerDetails.tsx
+++ src/components/OwnersComponents/OwnerDetail/OwnerDetails.tsx
@@ -1,11 +1,32 @@
-import { Fragment } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
-import { OWNER_PARAM_NAME } from "../../../config";
+import { MARGIN_SPACE, OWNER_PARAM_NAME } from "../../../config";
+import { IOwners } from "../../../interfaces/Iowners";
+import { getSingleOwner } from "../../../supabaseCall/owners/getSingleOwner";
+import { createOwnerData } from "./createOwnerContentData";
+import { Box, Divider, Heading } from "@chakra-ui/react";
+import { InformationBoxWithHeader } from "../../Common/InformationBoxWithHeader/InformationBoxWithHeader";
 
 export const OwnerDetails = () => {
     const [searchParams] = useSearchParams();
     const OWNER_NUMBER = searchParams.get(OWNER_PARAM_NAME) as string
+    const [owner, setOwner] = useState<IOwners | null>(null)
+    useEffect(()=>{
+        const getOwner = async ()=> {
+            const ownerData = await getSingleOwner(OWNER_NUMBER);
+            setOwner(ownerData);
+        }
+        getOwner();
+    })
+    if(!owner) return null;
+    const onwerData = createOwnerData(owner);
+
     return <Fragment>
-        owner details (number) {OWNER_NUMBER}
+        <Box my={MARGIN_SPACE}>
+            <InformationBoxWithHeader header={"Owner details"} content={onwerData}/>
+        </Box>
+        <Divider my={MARGIN_SPACE}/>
+        <Heading size="md" my={MARGIN_SPACE}>Owner estates list</Heading>
+        {/* list of owner estates */}
     </Fragment>
 }
